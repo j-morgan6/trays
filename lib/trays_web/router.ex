c616dc882bc615f430a495d15a12f01c54f805e6
j-morgan6 <jsm10242000@gmail.com>
2025-10-03 19:23:08 -0400

    upadted sobelow

--- lib/trays_web/router.ex
+++ lib/trays_web/router.ex
@@ -7,7 +7,7 @@ defmodule TraysWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {TraysWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'"}
   end
 
   pipeline :api do
