3ad56afb388169e7641acbe25f41bde3adfd7c08
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-10-10 10:28:26 -0400

    added some users to the system

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Trays.Repo
+alias Trays.Accounts.User
+
+Repo.delete_all(User)
+
+Code.require_file("seeds/users.exs", __DIR__)
---
+++ priv/repo/seeds/users.exs
@@ -0,0 +1,42 @@
+
+alias Trays.Accounts
+
+Accounts.register_user(%{
+  name: "Cheezy Morgan",
+  email: "cheezy@letstango.ca",
+  phone_number: "647-992-2499",
+  password: "change_me_cheezy",
+  type: :admin
+})
+
+Accounts.register_user(%{
+  name: "Ardita Karaj",
+  email: "ardita@letstango.ca",
+  phone_number: "416-230-4519",
+  password: "change_me_ardita",
+  type: :admin
+})
+
+Accounts.register_user(%{
+  name: "Joseph Morgan",
+  email: "joser.morgan6@gmail.com",
+  phone_number: "647-594-4519",
+  password: "change_me_joseph",
+  type: :admin
+})
+
+Accounts.register_user(%{
+  name: "Debbie Dore",
+  email: "debbie@trays.ca",
+  phone_number: "123-456-7890",
+  password: "Debbie@123Pain",
+  type: :merchant
+})
+
+Accounts.register_user(%{
+  name: "Curious Customer",
+  email: "Customer@trays.ca",
+  phone_number: "123-456-7890",
+  password: "customer@123Trays",
+  type: :customer
+})
